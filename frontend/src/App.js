5685bd9785795cabe96dc8b65b2bf3f6742186a2
ask0123 <116483519+ask0123@users.noreply.github.com>
2024-06-30 14:07:16 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RegistrationForm from './components/RegistrationForm';
+import LoginForm from './components/LoginForm';
+import MovieList from './components/MovieList';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<RegistrationForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/movies" element={<MovieList />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
